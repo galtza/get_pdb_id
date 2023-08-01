ded9cb5314292f13b9feeafa494d82528f1bdbe7
Galtza <igatlza@gmail.com>
2023-08-01 23:37:54 +0200

    Added option to generate SymStore Folder Ids

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,38 @@
 #include <atlbase.h>
 #include <iostream>
 
+using namespace std;
+
 using DllGetClassObject_t = HRESULT(__stdcall*)(REFCLSID rclsid, REFIID riid, LPVOID* ppv);
 
-int wmain(int argc, wchar_t* argv[]) {
-    if (argc != 2) {
-        std::wcerr << L"Usage: " << argv[0] << L" <pdb-file>\n"
-                   << L"\n"
-                   << L"  <pdb-file>: The path to the PDB file to be loaded.\n"
-                   << L"\n"
-                   << L"Remarks:\n"
-                   << L"  The program prints the GUID of the PDB file in case of success.\n"
-                   << L"  In case of failure, the program prints nothing and returns -1.\n";
+bool process_arguments(int _argc, wchar_t* _argv[], wstring& _pdb_file) {
+    bool print_symstore_id = false;
+
+    for (int i = 1; i < _argc; ++i) {
+        wstring arg = _argv[i];
+        if (arg == L"-ssid" || arg == L"--symbol-store-id") {
+            print_symstore_id = true;
+        } else {
+            _pdb_file = arg;  // Assuming the PDB file is the only non-option argument
+        }
+    }
+
+    return print_symstore_id;
+}
+
+int wmain(int _argc, wchar_t* _argv[]) {
+    auto pdb_file          = wstring{};
+    auto print_symstore_id = process_arguments(_argc, _argv, pdb_file);
+    if (pdb_file.empty()) {
+        wcerr << L"Usage: " << _argv[0] << L" <pdb-file> [-ssid | --symbol-store-id]\n"
+              << L"\n"
+              << L"  pdb-file                   path to the PDB file\n"
+              << L"  -ssid, --symbol-store-id   print a SymStore-compatible ID\n"
+              << L"\n"
+              << L"Remarks:\n"
+              << L"  The program prints the GUID of the PDB file. If the -ssid or --symbol-store-id\n"
+              << L"  option is given, it prints a SymbolStore-compatible ID instead.\n"
+              << L"  If the program fails, it does not print anything and returns -1.\n";
         return -1;
     }
 
@@ -32,7 +53,7 @@ int wmain(int argc, wchar_t* argv[]) {
     ok = ok && (get_class_obj = (DllGetClassObject_t)GetProcAddress(msdia140, "DllGetClassObject")) != nullptr;  // Get a pointer to the DllGetClassObject function
     ok = ok && !FAILED(get_class_obj(__uuidof(DiaSource), IID_IClassFactory, (void**)&factory));                 // Use `DllGetClassObject` to get a class factory for the `IDiaSource` class
     ok = ok && !FAILED(factory->CreateInstance(NULL, __uuidof(IDiaDataSource), (void**)&source_interface));      // Create the data source
-    ok = ok && !FAILED(source_interface->loadDataFromPdb(argv[1]));                                              // Load the PDB
+    ok = ok && !FAILED(source_interface->loadDataFromPdb(_argv[1]));                                             // Load the PDB
     ok = ok && !FAILED(source_interface->openSession(&session_interface));                                       // Open the session that will query the PDB data
     ok = ok && !FAILED(session_interface->get_globalScope(&global_scope_interface));                             // Get the global scope where all the types live
     ok = ok && global_scope_interface->get_guid(&guid) == S_OK;                                                  // Grab the GUID for debugging purposes
@@ -46,7 +67,7 @@ int wmain(int argc, wchar_t* argv[]) {
                guid.Data4[0], guid.Data4[1], guid.Data4[2], guid.Data4[3],
                guid.Data4[4], guid.Data4[5], guid.Data4[6], guid.Data4[7]);
 
-        if (age) {
+        if (print_symstore_id && age) {
             printf("%X", age);
         }
         printf("\n");
